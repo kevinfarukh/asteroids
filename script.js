11496bdc98a86af9b7c8b4bb2c9fcc75b997c265
kevinfarukh <kevinfarukh@gmail.com>
2025-01-19 08:10:36 -0500

    creating projectiles

--- script.js
+++ script.js
@@ -40,6 +40,34 @@ class Player {
     this.position.y += this.velocity.y;
   }
 }
+
+class Projectile {
+  constructor({ position, velocity }) {
+    this.position = position;
+    this.velocity = velocity;
+    this.radius = 5;
+  }
+  draw() {
+    context.beginPath();
+    context.arc(
+      this.position.x,
+      this.position.y,
+      this.radius,
+      0,
+      Math.PI * 2,
+      false
+    ); //draw circle
+    context.closePath();
+    context.fillStyle = "white";
+    context.fill();
+  }
+  update() {
+    this.draw();
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+  }
+}
+
 const player = new Player({
   position: { x: canvas.width / 2, y: canvas.height / 2 },
   velocity: { x: 0, y: 0 },
@@ -63,12 +91,30 @@ const keys = {
 const SPEED = 3;
 const ROTATION = 0.04;
 const FRICTION = 0.97;
+const PROJECTILE_SPEED = 4;
+
+const projectiles = [];
 
 function animate() {
   window.requestAnimationFrame(animate);
   context.fillStyle = "black";
   context.fillRect(0, 0, canvas.width, canvas.height); // Crear el rectangulo y el tamaño, 1. lugar x, 2. lugar y, 3. width, 4. height
+
   player.update();
+
+  for (let i = projectiles.length - 1; i >= 0; i--) {
+    const projectile = projectiles[i];
+    projectile.update();
+    // Eliminar del array para no tener basura coleccionandose
+    if (
+      projectile.position.x + projectile.radius < 0 ||
+      projectile.position.x - projectile.radius > canvas.width ||
+      projectile.position.y - projectile.radius > canvas.height ||
+      projectile.position.y + projectile.radius < 0
+    ) {
+      projectiles.splice(i, 1);
+    }
+  }
 
   // player.velocity.x = 0;
   // player.velocity.y = 0;
@@ -104,6 +150,20 @@ window.addEventListener("keydown", (e) => {
       break;
     case "KeyD":
       keys.d.pressed = true;
+      break;
+    case "Space":
+      projectiles.push(
+        new Projectile({
+          position: {
+            x: player.position.x + Math.cos(player.rotation) * 30, // sumado con el coseno hace que el proyectil se dispare en la rotación y el 30 es la diferencia para que no dispare desde el centro sino desde la cabeza del jugador
+            y: player.position.y + Math.sin(player.rotation) * 30,
+          },
+          velocity: {
+            x: Math.cos(player.rotation) * PROJECTILE_SPEED, // para que dispare hacia la dirección
+            y: Math.sin(player.rotation) * PROJECTILE_SPEED,
+          },
+        })
+      );
       break;
   }
 });
